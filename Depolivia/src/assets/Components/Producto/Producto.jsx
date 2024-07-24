acc293b2d941b541f0e530d5a7632e524eb29cdb
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2024-07-24 18:50:10 -0300

    create product

--- Depolivia/src/assets/Components/Producto/Producto.jsx
+++ Depolivia/src/assets/Components/Producto/Producto.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getProducto } from '../../../Redux/Actions/action';
+import './Producto.css';
 
 function Producto() {
   const navigate = useNavigate();
@@ -14,28 +15,35 @@ function Producto() {
   }, [dispatch]);
 
   return (
-    <div className=''>
-      {/* Mapea los productos y renderiza cada uno */}
-      {allProducto.length > 0 ? (
-        allProducto.map((product) => (
-          <div key={product.id} className='product-card'>
-            <h2>{product.denomination}</h2>
-            <p>Category: {product.category}</p>
-            <p>Stock: {product.stock}</p>
-            <p>Price: ${product.price}</p>
-            <img src={product.image} alt={product.denomination} />
-          </div>
-        ))
-      ) : (
-        <p>No hay productos</p> // Mensaje en caso de que no haya productos
-      )}
-      <div>
+    <div className="container">
+      <div className="row">
+        {/* Mapea los productos y renderiza cada uno */}
+        {allProducto.length > 0 ? (
+          allProducto.map((product) => (
+            <div className="col-md-3 mb-3">
+              <div className="card">
+                <img src={product.image} className="card-img-top fixed-img-size" alt= {product.denomination}   />
+                <div key={product.id} className="card-body text-white bg-dark">
+                  <h5 className="card-title">{product.denomination}</h5>
+                  <p className="card-text">Categoría: {product.category}</p>
+                  <p className="card-text">Stock: {product.stock}</p>
+                  <p className="card-text">Precio: $ {product.price}</p>
+                  <p className="card-text">ID:{product.id}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No hay productos</p> // Mensaje en caso de que no haya productos
+        )}
+      </div>
+      <div className="mt-3">
         <button type="button" className="btn btn-primary" onClick={() => navigate('/create/producto')}>
-          Crear Producto
+          CREAR PRODUCTO
         </button>
       </div>
     </div>
   );
-}
+};
 
-export default Producto;
+export default Producto
